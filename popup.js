ef120b0d14dca72fe9da788c12e4a294532de4a8
shilpa syal <shilpasyal55@gmail.com>
2019-03-18 18:03:59 +0530

    toolbar changed

--- popup.js
+++ popup.js
@@ -32,6 +32,16 @@ markPageButton.addEventListener('click',function() {
     });
 });
 
+let blogscount = JSON.parse(localStorage.getItem('blogs'));
+
+let uncompleted = blogscount.filter((e) => JSON.parse(e).mark == false).length
+let completed = blogscount.filter((e) => JSON.parse(e).mark == true).length
+let total = blogscount.length
+
+document.getElementById('total').innerText = total;
+document.getElementById('complete').innerText = completed;
+document.getElementById('uncomplete').innerText = uncompleted;
+
 let showAllButton = document.getElementById("showAll");
 showAllButton.addEventListener('click',function(){
     chrome.tabs.create({ 'url': chrome.extension.getURL('index.html'), 'selected': true});
